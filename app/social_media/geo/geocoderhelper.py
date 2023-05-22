e82d17035938a19724a86b82c26f0e97d7857273
Illia Sazhko <isazhko@mindk.com>
2023-05-22 21:01:11 +0300

    [fix] make geocoder restartable

--- app/social_media/geo/geocoderhelper.py
+++ app/social_media/geo/geocoderhelper.py
@@ -1,6 +1,9 @@
 import dataclasses
 import hashlib
-from typing import Optional, Union, List
+import logging
+from typing import Optional, Union, List, Callable
+
+import geopy.exc
 from django.core.cache import cache
 
 from django.conf import settings
@@ -8,6 +11,8 @@ from django.contrib.gis.geos import GEOSGeometry, MultiPolygon, Point
 from geopy import Location
 from geopy import Nominatim
 
+
+logger = logging.getLogger(__name__)
 
 @dataclasses.dataclass
 class GeoCoderLookup:
@@ -29,24 +34,36 @@ class GeoCoderHelper:
 
     def geocode(self,
                 query: Union[str],
-                country_codes: Union[str, List[str]] = None) -> Optional[GeoCoderLookup]:
+                country_codes: Union[str, List[str]] = None,
+                max_retries: int = 10,
+                delay: int = 60) -> Optional[GeoCoderLookup]:
 
         cache_key = f'nominatium_location_{hashlib.md5(query.encode()).hexdigest()}'
 
         cache_item = cache.get(cache_key)
 
+        lookup_result: Optional[Union[Location, False]] = None
+
         if cache_item is None:
-            lookup_result: Location = self.get_geocoder_inst().geocode(
-                query=query,
-                geometry='wkt',
-                language='ru',
-                country_codes=country_codes
-            )
+            for attempt in range(max_retries):
+                try:
+                    lookup_result = self.restartable_gecode(lambda: self.get_geocoder_inst().geocode(
+                        query=query,
+                        geometry='wkt',
+                        language='ru',
+                        country_codes=country_codes
+                    ), delay=delay, max_retries=max_retries)
 
-            if not lookup_result:
-                lookup_result = False
 
-            cache.set(cache_key, lookup_result, 24 * 60 * 60)
+                    if not lookup_result:
+                        lookup_result = False
+
+                    cache.set(cache_key, lookup_result, 24 * 60 * 60)
+                except geopy.exc.GeocoderUnavailable:
+
+                    logger.error('Geocoder attempt failed failed')
+
+
         else:
             lookup_result = cache_item
 
@@ -58,7 +75,16 @@ class GeoCoderHelper:
             )
 
         return None
+    @staticmethod
+    def restartable_gecode(cb: Callable, delay: int, max_retries: int) -> Location:
+        for attempt in range(max_retries):
+            try:
+                return cb()
+            except geopy.exc.GeocoderUnavailable:
+                if (attempt + 1) == max_retries:
+                    raise
 
+                logger.error(f'Geocoder attempt {attempt + 1} failed failed, retry in {delay}s')
     @staticmethod
     def normalize_to_multipolygon(lookup: GeoCoderLookup):
         if not lookup.geometry:
